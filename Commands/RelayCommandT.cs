69817f78ab377c318698657d00484a3cf9a984e7
invisloa <91217120+invisloa@users.noreply.github.com>
2024-08-17 23:09:03 +0200

    RelayCommand and EventViewModels

---
+++ Commands/RelayCommandT.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TheKalendarzyk.Commands
+{
+    public class RelayCommand<T> : ICommand
+    {
+        #region Fields
+
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+        private event EventHandler canExecuteChanged;
+
+        #endregion
+
+        #region Constructors
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        #endregion
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { canExecuteChanged += value; }
+            remove { canExecuteChanged -= value; }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        public void RaiseCanExecuteChanged()
+        {
+            canExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
+    }
+
+}
